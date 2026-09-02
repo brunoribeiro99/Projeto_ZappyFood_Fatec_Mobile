88ef43320305780a7b202c389181820cb2275eeb
Bruno Ribeiro <bhribeiro99@gmail.com>
2026-09-01 22:14:59 -0300

    Atualização do bd

--- src/screen/services/firebaseConfig.tsx
+++ src/screen/services/firebaseConfig.tsx
@@ -1,29 +1,26 @@
-import { initializeApp } from 'firebase/app';
-import { getDatabase } from 'firebase/database';
-import {
-  initializeAuth,
-  getReactNativePersistence,
-} from 'firebase/auth';
-import AsyncStorage from '@react-native-async-storage/async-storage';
- 
+//Inicialização da conexão com Firebase
+import { initializeApp } from "firebase/app";
+//Inicializa a conexão com o banco de dados em tempo real do Firebase
+import { getDatabase } from "firebase/database";
+//Inicializa com a autenticação do Firebase
+import { initializeAuth } from "firebase/auth";
+
 const firebaseConfig = {
-  apiKey: 'SUA_API_KEY',
-  authDomain: 'SEU_PROJETO.firebaseapp.com',
-  databaseURL: 'https://SEU_PROJETO-default-rtdb.firebaseio.com',
-  projectId: 'SEU_PROJETO',
-  storageBucket: 'SEU_PROJETO.firebasestorage.app',
-  messagingSenderId: 'SEU_MESSAGING_SENDER_ID',
-  appId: 'SEU_APP_ID',
+  apiKey: "AIzaSyA0xElXomsvRmhZ5rr1NIAlYSK_WSgrdLo",
+  authDomain: "zappyfood-b0671.firebaseapp.com",
+  projectId: "zappyfood-b0671",
+  storageBucket: "zappyfood-b0671.firebasestorage.app",
+  messagingSenderId: "526213495204",
+  appId: "1:526213495204:web:98a9f9c3f65c3081ece931",
+  baseURL: "https://zappyfood-b0671-default-rtdb.firebaseio.com/",
 };
- 
+
 const app = initializeApp(firebaseConfig);
- 
+
 // Firebase Authentication
-export const auth = initializeAuth(app, {
-  persistence: getReactNativePersistence(AsyncStorage),
-});
- 
+export const auth = initializeAuth(app);
+
 // Realtime Database
 export const database = getDatabase(app);
- 
+
 export default app;
